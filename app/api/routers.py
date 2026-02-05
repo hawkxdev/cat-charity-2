2ed783f3a5a57379316a35bf04e3f164e4d24c97
Sergey Sokolkin <hawkxdev@gmail.com>
2026-02-05 21:13:51 +0300

    feat(auth): add auth routers and lifespan

--- app/api/routers.py
+++ app/api/routers.py
@@ -2,7 +2,9 @@
 
 from fastapi import APIRouter
 
-from app.api.endpoints import charity_project_router, donation_router
+from app.api.endpoints import (
+    charity_project_router, donation_router, user_router,
+)
 
 main_router = APIRouter()
 
@@ -16,3 +18,4 @@ main_router.include_router(
     prefix='/donation',
     tags=['Donations'],
 )
+main_router.include_router(user_router)
